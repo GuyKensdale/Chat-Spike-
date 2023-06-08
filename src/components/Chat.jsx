65f64432923ded4a44e4a0bab2c08da472323ec3
JoshuaGoodwill <jgoodwill1234@gmail.com>
2023-06-08 12:45:13 +0100

    Finished messaging app

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import Message from "./Message";
 import { db } from "../firebase";
 import { query, collection, orderBy, onSnapshot } from "firebase/firestore";
+import SendMessage from "./SendMessage";
 
 const style = {
   main: `flex flex-col p-[10px] relative`,
@@ -18,6 +19,7 @@ const Chat = () => {
       querySnapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
+      console.log(messages);
       setMessages(messages);
     });
     return () => unsubscribe();
@@ -28,9 +30,11 @@ const Chat = () => {
       <main className={style.main}>
         {messages &&
           messages.map((message) => {
-            <Message key={message.id} message={message} />;
+            return <Message key={message.id} message={message} />;
           })}
       </main>
+
+      <SendMessage scroll={scroll}/>
 
       <span ref={scroll}></span>
     </>
